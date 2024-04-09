4bb8eeaf9e650a5ba56978bdcce65088f4c60633
whitebit-robot <robot@whitebit.com>
2024-04-09 12:52:24 +0200

    change open/modify date format to float64 in open positions

--- module/account/collateral/position_open.go
+++ module/account/collateral/position_open.go
@@ -7,21 +7,21 @@ import (
 const openPositionsEndpointUrl = "/api/v4/collateral-account/positions/open"
 
 type OpenPosition struct {
-	PositionID        int64  `json:"positionId"`
-	Market            string `json:"market"`
-	OpenDate          string `json:"openDate"`
-	ModifyDate        string `json:"modifyDate"`
-	Amount            string `json:"amount"`
-	BasePrice         string `json:"basePrice"`
-	LiquidationPrice  string `json:"liquidationPrice"`
-	Leverage          string `json:"leverage"`
-	Pnl               string `json:"pnl"`
-	PnlPercent        string `json:"pnlPercent"`
-	Margin            string `json:"margin"`
-	FreeMargin        string `json:"freeMargin"`
-	Funding           string `json:"funding"`
-	UnrealizedFunding string `json:"unrealizedFunding"`
-	LiquidationState  string `json:"liquidationState"`
+	PositionID        int64   `json:"positionId"`
+	Market            string  `json:"market"`
+	OpenDate          float64 `json:"openDate"`
+	ModifyDate        float64 `json:"modifyDate"`
+	Amount            string  `json:"amount"`
+	BasePrice         string  `json:"basePrice"`
+	LiquidationPrice  string  `json:"liquidationPrice"`
+	Leverage          string  `json:"leverage"`
+	Pnl               string  `json:"pnl"`
+	PnlPercent        string  `json:"pnlPercent"`
+	Margin            string  `json:"margin"`
+	FreeMargin        string  `json:"freeMargin"`
+	Funding           string  `json:"funding"`
+	UnrealizedFunding string  `json:"unrealizedFunding"`
+	LiquidationState  string  `json:"liquidationState"`
 }
 
 type openPositionsEndpoint struct {
